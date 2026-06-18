c979e7240032d994cb113f6b30f9d3fe4e51f3a4
Zombie clanx <85741103+NOT-ZCX@users.noreply.github.com>
2026-06-18 15:58:28 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import { logger } from "@revenge-mod/api";
+import * as devMode from "./plugins/devMode";
+// Import new plugins here as you create them
+
+const plugins = [
+    devMode
+    // Add new plugins to this list
+];
 
 export default {
     onLoad: () => {
-        logger.log("My Plugin has loaded!");
-        // Add your patches or logic here
+        for (const plugin of plugins) {
+            try {
+                plugin.onLoad?.();
+            } catch (e) {
+                logger.error("Failed to load plugin:", e);
+            }
+        }
     },
     onUnload: () => {
-        logger.log("My Plugin has been unloaded!");
-        // Remove patches or cleanup here
+        for (const plugin of plugins) {
+            try {
+                plugin.onUnload?.();
+            } catch (e) {
+                logger.error("Failed to unload plugin:", e);
+            }
+        }
     }
 };
